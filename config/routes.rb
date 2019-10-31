793fadc52931964fbf18e398587d6e7870ae24bc
Francois Blanc <francois.blanc73@gmail.com>
2019-10-31 15:57:38 +0100

    Chore: Setup background jobs processing with sidekiq and redis

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  # TODO: Find another way to protect this route than admin user
+  # require "sidekiq/web"
+  # authenticate :user, lambda { |u| u.admin } do
+  #   mount Sidekiq::Web => '/sidekiq'
+  # end
 end
