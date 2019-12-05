e8b8c9cc288727cd9e6d41152bf491c975dae14d
Francois Blanc <francois.blanc73@gmail.com>
2019-12-05 08:36:23 +0100

    Refactor: Use DateTime instead of Date for associates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_141357) do
+ActiveRecord::Schema.define(version: 2019_12_05_071106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,13 +20,13 @@ ActiveRecord::Schema.define(version: 2019_11_28_141357) do
     t.integer "total_associates"
     t.string "last_associate_name_1"
     t.integer "last_associate_shares_1"
-    t.date "last_associate_date_1"
+    t.datetime "last_associate_date_1"
     t.string "last_associate_name_2"
     t.integer "last_associate_shares_2"
-    t.date "last_associate_date_2"
+    t.datetime "last_associate_date_2"
     t.string "last_associate_name_3"
     t.integer "last_associate_shares_3"
-    t.date "last_associate_date_3"
+    t.datetime "last_associate_date_3"
   end
 
   create_table "entrepreneurs", force: :cascade do |t|
